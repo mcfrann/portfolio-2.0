08619870eb9768d1ac0ab23e502337e21375914a
Francesca McConnell <fmcconnell59@gmail.com>
2022-07-07 15:53:31 -0600

    Add fade-in on scroll

--- src/Components/ProjectTile/ProjectTile.tsx
+++ src/Components/ProjectTile/ProjectTile.tsx
@@ -1,16 +1,45 @@
 import './ProjectTile.scss'
+import React, { useState, useRef, useEffect } from 'react'
 
 interface ProjectInfo {
   key: number
   id: number
   name: string
+  repo: string
 }
 
 const ProjectTile = (props: ProjectInfo) => {
+  const [isVisible, setIsVisible] = useState(true)
+  const domRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const current = domRef.current
+    if (!current) return
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setIsVisible(entry.isIntersecting))
+    })
+    observer.observe(domRef.current)
+    return () => {
+      const current = domRef.current
+      if (!current) return
+      return observer.unobserve(domRef.current)
+    }
+  }, [])
+
   return (
-    <section className='project-tile'>
-      <h1 className='proj-name'>{props.name}</h1>
-    </section>
+    <div
+      className={`project-tile ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      <a
+        href={props.repo}
+        target='_blank'
+        className='project-link'
+        style={{ textDecoration: 'none', color: 'black' }}
+      >
+        <h1 className='proj-name'>{props.name}</h1>
+      </a>
+    </div>
   )
 }
 
