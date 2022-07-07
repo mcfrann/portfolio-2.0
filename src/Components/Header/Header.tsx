3eb7ce931247685aabcbc289fe37969e22d45e84
Francesca McConnell <fmcconnell59@gmail.com>
2022-07-06 23:40:12 -0600

    Switch to link instead of useNavigate

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import './Header.scss'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
       <div className='top-nav header-detail'>
         <h1>Francesca McConnell</h1>
-        <h1>About</h1>
+        <Link to='/about' style={{ textDecoration: 'none', color: 'black' }}>
+          <h1 className='about'>About</h1>
+        </Link>
       </div>
       <h1 className='header-detail'>Web development & Design</h1>
       <h1 className='header-detail'>Denver CO - Los Angeles CA</h1>
