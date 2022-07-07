91cf2dcb3ade85adb6336555a5d4a9e6554bbe3d
Francesca McConnell <fmcconnell59@gmail.com>
2022-07-06 21:56:18 -0600

    Remove default, import header, main, video

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+const video = require('./background-vid.mp4')
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='video-wrapper'>
+        <video id='background-video' autoPlay loop muted>
+          <source src={video} type='video/mp4' />
+        </video>
+      </div>
+      <Header />
+      <Main />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
