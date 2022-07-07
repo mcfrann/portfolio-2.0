32ec553200190765eb642022c8b6fb7e339dd972
Francesca McConnell <fmcconnell59@gmail.com>
2022-07-06 21:57:48 -0600

    Create header component

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='top-nav header-detail'>
+        <h1>Francesca McConnell</h1>
+        <h1>About</h1>
+      </div>
+      <h1 className='header-detail'>Web development & Design</h1>
+      <h1 className='header-detail'>Denver CO - Los Angeles CA</h1>
+    </header>
+  )
+}
+
+export default Header
