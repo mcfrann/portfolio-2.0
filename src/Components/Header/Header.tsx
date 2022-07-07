109effbbcacc00aece9beedd4b674991441c3933
Francesca McConnell <fmcconnell59@gmail.com>
2022-07-07 16:52:47 -0600

    Tinker with classnames

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className='top-nav header-detail'>
         <h1 className='page-title'>Francesca McConnell</h1>
         <Link to='/about' style={{ textDecoration: 'none', color: 'black' }}>
-          <h1 className='nav-link'>About</h1>
+          <h1 className='nav-link about-link-one'>About</h1>
         </Link>
       </div>
       <div className='header-detail'>
@@ -20,6 +20,11 @@ const Header = () => {
           <img className='gif' src={rightArrow} />
           Los Angeles CA
         </h1>
+      </div>
+      <div className='header-detail about-link-two'>
+        <Link to='/about' style={{ textDecoration: 'none', color: 'black' }}>
+          <h1 className='nav-link about-link-two'>About</h1>
+        </Link>
       </div>
     </header>
   )
